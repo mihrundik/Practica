4efa1ba2fd017b50cc8a911b171828c90b2478ad
mihrundik <mihrundik@mail.ru>
2025-11-11 23:18:51 +0700

    homework_11-11-2025

--- src/Fibonacci_homework/Fibonacci.java
+++ src/Fibonacci_homework/Fibonacci.java
@@ -3,6 +3,7 @@ package Fibonacci_homework.calc;
 import java.util.Scanner;
 
 public class Fibonacci {
+
     public static void main(String[] args) {
         System.out.println("Введите число х? до которого необходимо вычислить число Фибоначчи: ");
         Scanner scanner = new Scanner(System.in);
@@ -11,4 +12,5 @@ public class Fibonacci {
         Calculation calc = new Calculation();
         System.out.print(calc.sum(x));
     }
+
 }
