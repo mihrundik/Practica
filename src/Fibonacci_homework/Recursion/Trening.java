6970c7aa02dc79bab130344131dc871090adbf46
mihrundik <mihrundik@mail.ru>
2025-11-12 23:39:20 +0700

    homework_12-11-2025

---
+++ src/Fibonacci_homework/Recursion/Trening.java
@@ -0,0 +1,14 @@
+package Fibonacci_homework.Recursion;
+
+import Fibonacci_homework.Recursion.TreningSecond.Second;
+
+public class Trening {
+    public static void main(String[] args) {
+
+        // Дано натуральное число n. Выведите все числа от 1 до n.
+        Second x = new Second();
+        x.first(1, 5);
+
+
+    }
+}
---
+++ src/Fibonacci_homework/Recursion/TreningSecond/Second.java
@@ -0,0 +1,13 @@
+package Fibonacci_homework.Recursion.TreningSecond;
+
+public class Second {
+
+    public void first(int count, int n) {
+        if (n > count - 1) {
+            System.out.print(count + " ");
+            first(count + 1, n);
+        }
+
+
+    }
+}
